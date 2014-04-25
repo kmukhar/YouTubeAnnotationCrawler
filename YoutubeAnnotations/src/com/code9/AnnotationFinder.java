db56344f88150e8d3a84087b5eba4b913351812b
kevin_mukhar <kevin.mukhar@gmail.com>
2014-04-24 23:58:19 -0700

    updated the code

--- YoutubeAnnotations/src/com/code9/AnnotationFinder.java
+++ YoutubeAnnotations/src/com/code9/AnnotationFinder.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Random;
 
 import javax.ws.rs.ServiceUnavailableException;
 import javax.ws.rs.client.Client;
@@ -22,65 +23,98 @@ import edu.princeton.cs.introcs.Out;
 
 public class AnnotationFinder {
     private final String BASE_URI = "https://www.googleapis.com";
-    
+
     private Hashtable<String, String> videoIds;
 
     private Hashtable<String, String> icarusIds;
+
+    private Random r = new Random(System.currentTimeMillis());
+
+    private int idleTime = 0;
 
     public AnnotationFinder() {
         videoIds = new Hashtable<>();
         icarusIds = new Hashtable<>();
     }
 
-    private void getAnnotations() {
+    private void getVideoIds() {
         getArchivedData();
 
         Client c = ClientBuilder.newClient();
-        WebTarget target = getTarget(c);
+        WebTarget target = getChannelListTarget(c);
 
         String responseMsg = target.request(MediaType.TEXT_PLAIN_TYPE).get(
-                        String.class);
+                String.class);
         Gson gson = new Gson();
         ChannelList list = gson.fromJson(responseMsg, ChannelList.class);
 
         String nextPageToken = list.getNextPageToken();
+
+        int count = 0;
 
         while (true) {
             List<Item> items = list.getItems();
             for (Item item : items) {
                 Id id = item.getId();
                 videoIds.put(id.getVideoId(), id.getVideoId());
+                System.out.println(++count + " of "
+                        + list.getPageInfo().getTotalResults() + ": "
+                        + id.getVideoId());
             }
-            
-            if (nextPageToken == null || nextPageToken.equals(""))
+
+            if (nextPageToken == null)
                 break;
-            
-            target = getTarget(c, nextPageToken);
-            try {
-                responseMsg = target.request(MediaType.TEXT_PLAIN_TYPE).get(String.class);
-            list = gson.fromJson(responseMsg, ChannelList.class);
-            nextPageToken = list.getNextPageToken();
-            } catch (ServiceUnavailableException e) {
-                break;
+
+            target = getChannelListTarget(c, nextPageToken);
+
+            sleep();
+
+            boolean tryRequest = true;
+
+            while (tryRequest) {
+                try {
+                    responseMsg = target.request(MediaType.TEXT_PLAIN_TYPE)
+                            .get(String.class);
+                    list = gson.fromJson(responseMsg, ChannelList.class);
+                    nextPageToken = list.getNextPageToken();
+                    tryRequest = false;
+                } catch (ServiceUnavailableException e) {
+                    idleTime += 60000;
+                    sleep(idleTime);
+                }
             }
-        } 
+        }
 
         writeData();
     }
 
-    private WebTarget getTarget(Client c, String nextPageToken) {
-        WebTarget target = getTarget(c);
+    private void sleep(int amount) {
+        try {
+            Thread.sleep(amount);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sleep() {
+        sleep(r.nextInt(250) + 100);
+    }
+
+    private WebTarget getChannelListTarget(Client c, String nextPageToken) {
+        WebTarget target = getChannelListTarget(c);
         target.queryParam("pageToken", nextPageToken);
         return target;
     }
 
-    private WebTarget getTarget(Client c) {
+    private WebTarget getChannelListTarget(Client c) {
         WebTarget target = c.target(BASE_URI);
         target = target.path("youtube/v3/search");
-        target = target.queryParam("key",
-                        "AIzaSyAGBmy6sp2E68-RRPYfsx03eZd88GPHjKI");
+        // GoogleApiKey.key is a String that is your Google API Key
+        // See https://developers.google.com/console/help/#WhatIsKey
+        target = target.queryParam("key", GoogleApiKey.key);
         target = target.queryParam("channelId", "UC_x5XG1OV2P6uZZ5FSM9Ttw")
-                        .queryParam("part", "id");
+                .queryParam("part", "id");
+        target = target.queryParam("maxResults", 50);
         target = target.queryParam("order", "date");
         return target;
     }
@@ -92,6 +126,7 @@ public class AnnotationFinder {
         try {
             fos = new FileOutputStream(f);
             out = new Out(fos);
+
             for (String o : videoIds.values())
                 out.println(o);
         } catch (FileNotFoundException e1) {
@@ -139,6 +174,6 @@ public class AnnotationFinder {
 
     public static void main(String[] args) {
         AnnotationFinder af = new AnnotationFinder();
-        af.getAnnotations();
+        af.getVideoIds();
     }
 }
